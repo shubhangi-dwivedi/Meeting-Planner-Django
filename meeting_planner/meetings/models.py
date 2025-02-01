057bc1ae4f94755db86b8de26fcaa81672fbf669
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-01 19:05:12 +0530

    added new Room model class

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,5 +1,16 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 from datetime import time
+
+class Room(models.Model):
+    room = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.room} at {self.floor} on {self.room_number}"
+
 
 # Create your models here.
 class Meeting(models.Model):
@@ -8,5 +19,10 @@ class Meeting(models.Model):
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
 
+    #foreign key
+    #this will help us select the room when we create a meeting
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) #this will hold the ID of the room obj that the meeting ref
+
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
+
