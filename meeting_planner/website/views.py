4706aa8c7bf6fc5a057451e7c2bd760b8e5767ae
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-02 01:23:24 +0530

    added templates folder

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 
+
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html")
 
 def date(request):
     return HttpResponse("Tis page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("About the page")
