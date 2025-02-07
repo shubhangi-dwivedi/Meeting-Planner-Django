7a32025d2aed32bd835731dec31f6e1bdab77ba7
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-08 02:25:12 +0530

    added templates

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
+from meetings.models import Meeting
 
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"num_meetings" : Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("Tis page was served at " + str(datetime.now()))
